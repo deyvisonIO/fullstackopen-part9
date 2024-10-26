826a5cc1c68f26e1f554d1eeb735cdfed004725b
Deyvison Iury <iurydeyvison@gmail.com>
2024-10-26 14:12:35 -0300

    Exercises 9.20

--- flight-diary/frontend/src/App.tsx
+++ flight-diary/frontend/src/App.tsx
@@ -42,30 +42,63 @@ function DiaryForm({ addDiary }: { addDiary: (diaryEntrie: Diary) => void }) {
     axios.post("http://localhost:3000/api/diaries", { date, visibility, comment, weather })
       .then(response => response?.data?.error ? notify(response.data.error) : addDiary(response.data))
       .catch(error => notify(error.response.data))
-
-
   }
 
   function notify(content: string) {
     setNotification(content)
     setTimeout(() => setNotification(""), 5000)
   }
+
   return (
     <div>
       {notification ? <p>{notification}</p> : null}
       <form onSubmit={submitDiary}>
         <div>
           <label htmlFor="date">date</label>
-          <input type="text" name="date" id="date" />
+          <input type="date" name="date" id="date" />
         </div>
-        <div>
-          <label htmlFor="visibility">visibility</label>
-          <input type="text" name="visibility" id="visibility" />
-        </div>
-        <div>
-          <label htmlFor="weather">weather</label>
-          <input type="text" name="weather" id="weather" />
-        </div>
+        <fieldset>
+          <label>Visibility: </label>
+          <span>
+            <label htmlFor="great">great</label>
+            <input type="radio" name="visibility" value="great" id="great" />
+          </span>
+          <span>
+            <label htmlFor="good">good</label>
+            <input type="radio" name="visibility" value="good" id="good" />
+          </span>
+          <span>
+            <label htmlFor="ok">ok</label>
+            <input type="radio" name="visibility" value="ok" id="ok" />
+          </span>
+          <span>
+            <label htmlFor="poor">poor</label>
+            <input type="radio" name="visibility" value="poor" id="poor" />
+          </span>
+        </fieldset>
+        <fieldset>
+          <label>Weather: </label>
+          <span>
+            <label htmlFor="sunny">sunny</label>
+            <input type="radio" name="weather" value="sunny" id="sunny" />
+          </span>
+          <span>
+            <label htmlFor="rainy">rainy</label>
+            <input type="radio" name="weather" value="rainy" id="rainy" />
+          </span>
+          <span>
+            <label htmlFor="cloudy">cloudy</label>
+            <input type="radio" name="weather" value="cloudy" id="cloudy" />
+          </span>
+          <span>
+            <label htmlFor="stormy">stormy</label>
+            <input type="radio" name="weather" value="stormy" id="stormy" />
+          </span>
+          <span>
+            <label htmlFor="windy">windy</label>
+            <input type="radio" name="weather" value="windy" id="windy" />
+          </span>
+        </fieldset>
         <div>
           <label htmlFor="comment">comment</label>
           <input type="text" name="comment" id="comment" />
