bbec4df6019297c7e2786ad026413f7bbf95696e
Deyvison Iury <iurydeyvison@gmail.com>
2024-10-28 22:08:03 -0300

    Exercises 9.23-9.26

--- patientor/frontend/src/components/Entries.tsx
+++ patientor/frontend/src/components/Entries.tsx
@@ -1,10 +1,14 @@
+import { Favorite, MedicalServices, Work } from "@mui/icons-material";
 import { Diagnosis, Entry, HealthCheckEntry, HospitalEntry, OccupationalHealthcareEntry } from "../types";
+import { ReactNode } from "react";
+import { Button } from "@mui/material";
 
 export const Entries = ({ entries, diagnoses }: { entries: Entry[], diagnoses: Diagnosis[] }) => {
   return (
     <div>
       <h3>entries</h3>
       {entries.length > 0 && entries.map(entry => <EntryItem key={entry.id} entry={entry} diagnoses={diagnoses} />)}  
+      <Button color="primary" variant="contained">ADD NEW ENTRY</Button>
     </div>
   );
 };
@@ -13,35 +17,38 @@ export const Entries = ({ entries, diagnoses }: { entries: Entry[], diagnoses: D
 export const EntryItem = ({ entry, diagnoses }: { entry: Entry, diagnoses: Diagnosis[] }) => {
   switch (entry.type) {
     case ("Hospital"):
-      return <HospitalEntry entry={entry} diagnoses={diagnoses} />;
+      return <HospitalEntryItem entry={entry} diagnoses={diagnoses} />;
     case ("OccupationalHealthcare"):
-      return <OccupationalEntry entry={entry} diagnoses={diagnoses} />;
+      return <OccupationalEntryItem entry={entry} diagnoses={diagnoses} />;
     case ("HealthCheck"):
-      return <HealthCheckEntry entry={entry} diagnoses={diagnoses} />;
+      return <HealthCheckEntryItem entry={entry} diagnoses={diagnoses} />;
     default:
       return null; 
   }
 };
 
-const HospitalEntry = ({ entry, diagnoses }: { entry: HospitalEntry, diagnoses: Diagnosis[]  }) => {
+const HospitalEntryItem = ({ entry, diagnoses }: { entry: HospitalEntry, diagnoses: Diagnosis[] }) => {
   console.log("hospital:", entry);
 
   return (
-    <div>
-      <p>{entry.date} {entry.description}</p> 
+    <div style={{ border: "1px solid black", padding: "4px", margin: "8px 0 8px 0" }}>
+      <p>{entry.date} <MedicalServices /> </p> 
+      <p>{entry.description}</p>
       <ul>
         {entry.diagnosisCodes ? entry.diagnosisCodes.map(code => <li key={code}>{code} {diagnoses?.find(diagnosis => diagnosis.code === code)?.name ?? null}</li>) : null}
       </ul>
+      <span>diagnose by {entry.specialist}</span>
     </div> 
   );
 };
 
-const OccupationalEntry = ({ entry, diagnoses }: { entry: OccupationalHealthcareEntry, diagnoses: Diagnosis[] }) => {
+const OccupationalEntryItem  = ({ entry, diagnoses }: { entry: OccupationalHealthcareEntry, diagnoses: Diagnosis[] }) => {
   console.log("occupational:", entry);
 
   return (
-    <div>
-      <p>{entry.date} {entry.description}</p> 
+    <div style={{ border: "1px solid black", padding: "4px", margin: "8px 0 8px 0" }}>
+      <p>{entry.date} <Work /></p> 
+      <p>{entry.description}</p>
       <ul>
         {entry.diagnosisCodes ? entry.diagnosisCodes.map(code => <li key={code}>{code} {diagnoses?.find(diagnosis => diagnosis.code === code)?.name ?? null}</li>) : null}
       </ul>
@@ -49,15 +56,34 @@ const OccupationalEntry = ({ entry, diagnoses }: { entry: OccupationalHealthcare
   );
 };
 
-const HealthCheckEntry = ({ entry, diagnoses }: { entry: HealthCheckEntry, diagnoses: Diagnosis[] }) => {
+const HealthCheckEntryItem  = ({ entry, diagnoses }: { entry: HealthCheckEntry, diagnoses: Diagnosis[] }) => {
   console.log("health check:", entry);
+  let icon: ReactNode;
+
+  switch(entry.healthCheckRating) {
+    case 1:
+      icon = <Favorite color="success" />;
+      break;
+    case 2:
+      icon = <Favorite color="warning" />;
+      break;
+    case 3:
+      icon = <Favorite color="error" />;
+      break;
+    default:
+      icon = <Favorite />;
+      break;
+  }
 
   return (
-    <div>
-      <p>{entry.date} {entry.description}</p> 
+    <div style={{ border: "1px solid black", padding: "4px", margin: "8px 0 8px 0" }}>
+      <p>{entry.date} <MedicalServices /></p>
+      <p>{entry.description}</p>
+      {icon}
       <ul>
         {entry.diagnosisCodes ? entry.diagnosisCodes.map(code => <li key={code}>{code} {diagnoses?.find(diagnosis => diagnosis.code === code)?.name ?? null}</li>) : null}
       </ul>
+      <span>diagnose by {entry.specialist}</span>
     </div> 
   );
 };
